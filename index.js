0138266aba5e70245053fd9ed9e51369152ec7f9
Kelvin <kmr19972015@gmail.com>
2023-01-17 17:52:19 -0500

    feat: first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const OpenAI = require("openai");
+const { Configuration, OpenAIApi } = OpenAI;
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const port = 3001;
+
+const configuration = new Configuration({
+  organization: "org-Ro5K7oFHglEcxE03B7n9dVA4",
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+// const response = await openai.listEngines();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/", async (req, res) => {
+  const { message } = req.body;
+  try {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${message}`,
+      max_tokens: 1000,
+      temperature: 0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+    //   top_p: 1,
+    //   stop: ["\n"],
+    });
+
+    response?.data?.choices?.length >= 0
+      ? res.json({
+          message: response.data.choices[0].text,
+        })
+      : res.json({
+          message: "There are no choices",
+        });
+  } catch (error) {
+    console.log("error: ", error.message);
+    return res.json({
+      message: "No ha sido posible obtener resultados para esta busqueda",
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log("App listening on port " + port);
+});
--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+const App = () => {
+  const [message, setMessage] = useState();
+  const [response, setResponse] = useState();
+  const [isLoading, setIsLoading] = useState(false);
 
-function App() {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    const query = await fetch("http://localhost:3001/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message }),
+    });
+    const result = await query.json();
+    setResponse(result.message);
+    setIsLoading(false);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Pregunta cualquier cosa
+          </label>
+          <textarea
+            className="form-control"
+            placeholder="Pregunta algo..."
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          ></textarea>
+        </div>
+        <button className="btn te btn-primary btn md" type="submit">
+          Submit
+        </button>
+      </form>
+
+      {isLoading ? (
+        <div className="spinner-border text-primary my-3" role="status">
+          <span className="visually-hidden">Cargando...</span>
+        </div>
+      ) : (
+        <div>{response}</div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
